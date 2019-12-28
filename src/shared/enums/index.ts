b36d7266b131bfc7422c0fda14c1ae3a9d84ff20
Orion Kindel <cakekindel@gmail.com>
2019-12-28 13:30:34 -0700

    refactor: add time period helper

--- src/shared/enums/index.ts
+++ src/shared/enums/index.ts
@@ -1,5 +1,6 @@
 export * from './chicken-growth-state.enum';
 export * from './environment.enum';
+export * from './order.enum';
 export * from './slack-dm-command.enum';
 export * from './slack-interaction-id.enum';
-export * from './order.enum';
+export * from './time-period.enum';
---
+++ src/shared/enums/time-period.enum.ts
@@ -0,0 +1,6 @@
+export enum TimePeriod {
+    Today = 'Today',
+    Week = 'This Week',
+    Month = 'This Month',
+    AllTime = 'All Time',
+}
