c3afb6aeae69c3e4ed2419b045072a098ee05b1f
Orion Kindel <cakekindel@gmail.com>
2020-01-18 12:12:42 -0700

    slim up resource mapper

--- src/api/services/providers/resource-mappers/resource-mapper.base.ts
+++ src/api/services/providers/resource-mappers/resource-mapper.base.ts
@@ -5,15 +5,11 @@ export abstract class ResourceMapperBase<TVm, TEntity> {
     public abstract mapToViewModel(entity: TEntity): TVm;
 
     public mapMaybeToEntity(vm: Maybe<TVm>): Maybe<TEntity> {
-        const vmMaybe = Maybe.fromNullable(vm).join();
-
-        return vmMaybe.map(this.mapToEntity);
+        return vm.map(this.mapToEntity);
     }
 
     public mapMaybeToViewModel(entity: Maybe<TEntity>): Maybe<TVm> {
-        const entityMaybe = Maybe.fromNullable(entity).join();
-
-        return entityMaybe.map(this.mapToViewModel);
+        return entity.map(this.mapToViewModel);
     }
 
     public mapArrayToEntities(vms: TVm[]): TEntity[] {
