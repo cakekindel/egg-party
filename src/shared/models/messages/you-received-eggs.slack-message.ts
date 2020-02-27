ce0f714140bcf0ad45ee96378bbce18d57145fd9
Orion Kindel <cakekindel@gmail.com>
2020-02-26 17:12:26 -0700

    refactor received-eggs-message

--- src/shared/models/messages/you-received-eggs.slack-message.ts
+++ src/shared/models/messages/you-received-eggs.slack-message.ts
@@ -3,11 +3,11 @@ import { SlackBlockMessage } from '../slack/messages';
 export class YouReceivedEggsSlackMessage extends SlackBlockMessage {
     constructor(giverId: string, eggCount: number) {
         const plural = eggCount !== 1;
-        super(
-            [],
-            `<@${giverId}> gave you ${plural ? eggCount : 'an'} egg${
-                plural ? 's' : ''
-            }!`
-        );
+
+        const atGiver = `<@${giverId}>`;
+        const eggCountString = `${plural ? eggCount : 'an'}`;
+        const eggOrEggs = `egg${plural ? 's' : ''}`;
+
+        super([], `${atGiver} gave you ${eggCountString} ${eggOrEggs}!`);
     }
 }
